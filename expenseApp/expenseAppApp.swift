a0ab6182e5dfab5511770b50033a5dc60c466552
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-17 08:29:10 -0800

    Initial Commit

---
+++ expenseApp/expenseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  expenseAppApp.swift
+//  expenseApp
+//
+//  Created by ahmad kaddoura on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct expenseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
